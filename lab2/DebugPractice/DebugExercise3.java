8961f583e91f761d30186fb0e5966f7216a795db
Yangweiyi Li <lywy.amy@gmail.com>
2020-12-28 17:27:47 -0800

    Complete

--- lab2/DebugPractice/DebugExercise3.java
+++ lab2/DebugPractice/DebugExercise3.java
@@ -8,7 +8,7 @@ public class DebugExercise3 {
             String vendor = in.readString();
             String foodType = in.readString();
             double cost = in.readDouble();
-            int numAvailable = in.readInt();
+            int numAvailable = in.readInt(); //Kroger has a negative number of turnips.
             if (foodType.equals("turnip")) {
                 int newTotal = totalTurnips + numAvailable;
                 totalTurnips = newTotal;
