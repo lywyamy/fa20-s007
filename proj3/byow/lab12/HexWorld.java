c167c92841d9b123ac9c4adb7fcf4e522ae88995
Yangweiyi Li <lywy.amy@gmail.com>
2021-04-09 08:53:33 -0700

    complete 1st discussion

--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -16,15 +16,18 @@ public class HexWorld {
     private static final long SEED = 3722;
     private static final Random RANDOM = new Random(SEED);
 
+    // Instance Variables
+    private int length;
+    private int width, height;
+
+
     // Constructor that tessellates 19 hexagons of length s into one large hexagon
     public HexWorld(int s){
         if (s < 2) { throw new IllegalArgumentException("The length of an individual hexagon must be greater than or equal to 2"); }
+        this.length = s;
+        this.width = widthHexWorld();
+        this.height = heightHexWorld();
 
-        // initialize the tile rendering engine with a window of size width x height
-        TERenderer ter = new TERenderer();
-        int width = widthHexWorld(s);
-        int height = heightHexWorld(s);
-        ter.initialize(width, height);
 
     }
 
@@ -32,27 +35,27 @@ public class HexWorld {
 
     // Calculates the width of the HexWorld filled be hexagons of length s.
     // The width is comprised of 2 lengths and 3 longest rows (the middle) of a hexagon.
-    private int widthHexWorld(int s) { return s * 2 + longestRow(s) * 3; }
+    private int widthHexWorld() { return this.length * 2 + longestRow() * 3; }
 
     // Calculates the height of the HexWorld filled be hexagons of length s.
     // The height is comprised of 5 heights of a hexagon.
-    private int heightHexWorld(int s) { return highestColumn(s) * 5; }
+    private int heightHexWorld() { return highestColumn() * 5; }
 
     // Calculate the longest (widest) row of a hexagon with length s.
-    private int longestRow(int s) { return s * 3 - 2; }
+    private int longestRow() { return this.length * 3 - 2; }
 
     // Calculates the highest column of a hexagon with length s.
-    private int highestColumn(int s) { return s * 2; }
+    private int highestColumn() { return this.length * 2; }
 
     // Calculates the lower-left X-coordinate of the ith individual hexagon of length s.
     // The ordering is the level order, from bottom to top, left to right.
-    private int ithStartX(int s, int i) {
+    private int ithStartX(int i) {
 
         return -1;
     }
 
     // Calculates the lower-left X-coordinate of the ith individual hexagon of length s.
-    private int ithStartY(int s, int i) {
+    private int ithStartY(int i) {
 
         return -1;
     }
@@ -68,4 +71,20 @@ public class HexWorld {
         }
     }
 
+    private int getWidth() {
+        return this.width;
+    }
+    private int getHeight() {
+        return this.height;
+    }
+    public static void main(String[] args) {
+        HexWorld hw = new HexWorld(3);
+        // initialize the tile rendering engine with a window of size width x height
+        TERenderer ter = new TERenderer();
+        ter.initialize(hw.getWidth(), hw.getHeight());
+
+
+
+        // ter.renderFrame(randomTiles);
+    }
 }
