99769a216e97417a7a146421953baf55807931f8
lywyamy <lywy.amy@gmail.com>
2021-04-28 16:27:43 -0700

    add lab13

--- proj3/byow/lab13/MemoryGame.java
+++ proj3/byow/lab13/MemoryGame.java
@@ -7,11 +7,9 @@ import java.awt.Font;
 import java.util.Random;
 
 public class MemoryGame {
-    private int width;
-    private int height;
-    private int round;
-    private Random rand;
-    private boolean gameOver;
+    private final int width;
+    private final int height;
+    private final Random rand;
     private boolean playerTurn;
     private static final char[] CHARACTERS = "abcdefghijklmnopqrstuvwxyz".toCharArray();
     private static final String[] ENCOURAGEMENT = {"You can do this!", "I believe in you!",
@@ -25,7 +23,7 @@ public class MemoryGame {
         }
 
         long seed = Long.parseLong(args[0]);
-        MemoryGame game = new MemoryGame(40, 40, seed);
+        MemoryGame game = new MemoryGame(60, 60, seed);
         game.startGame();
     }
 
@@ -44,31 +42,82 @@ public class MemoryGame {
         StdDraw.enableDoubleBuffering();
 
         //TODO: Initialize random number generator
+        rand = new Random(seed);
     }
 
     public String generateRandomString(int n) {
         //TODO: Generate random string of letters of length n
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(CHARACTERS[rand.nextInt(CHARACTERS.length)]);
+        }
+        return sb.toString();
     }
 
     public void drawFrame(String s) {
         //TODO: Take the string and display it in the center of the screen
         //TODO: If game is not over, display relevant game information at the top of the screen
+        Font font = new Font("Monaco", Font.BOLD, 30);
+        StdDraw.setFont(font);
+        StdDraw.setPenColor(Color.WHITE);
+        StdDraw.clear(StdDraw.BLACK);
+        StdDraw.text((double) width / 2, (double) height / 2, String.valueOf(s));
+        StdDraw.show();
     }
 
     public void flashSequence(String letters) {
-        //TODO: Display each character in letters, making sure to blank the screen between letters
+        for (int i = 0; i < letters.length(); i++) {
+            drawFrame(String.valueOf(letters.charAt(i)));
+            StdDraw.pause(1000);
+            drawFrame("");
+            StdDraw.pause(500);
+        }
+        playerTurn = true;
     }
 
     public String solicitNCharsInput(int n) {
-        //TODO: Read n letters of player input
-        return null;
+        StringBuilder sb = new StringBuilder();
+        if (playerTurn) {
+            for (int i = 0; i < n; i++) {
+                if (StdDraw.hasNextKeyTyped()) {
+                    sb.append(StdDraw.nextKeyTyped());
+                    drawFrame(sb.toString());
+                    StdDraw.pause(200);
+                }
+            }
+        }
+        playerTurn = false;
+        return sb.toString();
     }
 
     public void startGame() {
-        //TODO: Set any relevant variables before the game starts
+        playerTurn = false;
+        boolean gameOver = false;
+        int round = 1;
 
-        //TODO: Establish Engine loop
+        drawFrame("Complete 5 rounds to win.");
+        StdDraw.pause(2000);
+        drawFrame(ENCOURAGEMENT[rand.nextInt(ENCOURAGEMENT.length)]);
+        StdDraw.pause(2000);
+        while (!gameOver && round < 6) {
+            StdDraw.pause(1000);
+            drawFrame("Round: " + round);
+            StdDraw.pause(1000);
+            String expected = generateRandomString(round);
+            flashSequence(expected);
+            StdDraw.pause(3000);
+
+            if (playerTurn) {
+                String inputString = solicitNCharsInput(expected.length());
+                if (inputString.equals(expected)) { round++; }
+                else { gameOver = true; }
+            }
+        }
+        if (round == 6) {
+            drawFrame("All rounds complete! You won!");
+        } else {
+            drawFrame("Game over. You will get it next time.");
+        }
+        StdDraw.pause(1000);
     }
-
 }
